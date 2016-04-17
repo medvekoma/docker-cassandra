91a43ec1607861412e17dd674b702fceef7178a7
Attila Szucs <attila.szucs@gmail.com>
2016-04-17 23:12:43 +0200

    add cassandra-driver

---
+++ nobel-app/routes/nobel.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const cassandra = require('cassandra-driver')
+const client = new cassandra.Client({ contactPoints: ['192.168.99.100'], keyspace: 'ks1'});
+
+function renderResult(res, err, title, result){
+  if (err) {
+    console.log(">>> Error: " + err)
+    res.render('index', {title: 'Error!'})
+  } else {
+    res.render('nobel', { title: title, rows: result.rows });
+  }
+}
+
+/* GET by year. */
+router.get('/year/:year', function(req, res, next) {
+  var year = parseInt(req.params.year)
+  console.log("Year: " + year)
+  const query = 'SELECT * FROM nobel_laureates WHERE year=?';
+  client.execute(query, [year], {prepare: true}, function(err, result) {
+    renderResult(res, err, 'Laureates in ' + year, result)
+  });
+});
+
+/* GET by country. */
+router.get('/country/:code', function(req, res, next) {
+  var code = req.params.code.toUpperCase()
+  console.log("Country code: " + code)
+  const query = 'SELECT * FROM nobel_laureates WHERE borncountrycode=?';
+  client.execute(query, [code], {prepare: true}, function(err, result) {
+    renderResult(res, err, 'Laureates from ' + code, result)
+  });
+});
+module.exports = router;
