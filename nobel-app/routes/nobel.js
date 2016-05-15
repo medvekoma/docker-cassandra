1b54f90713d525c1982c6a8524dc67bb7bc1054d
Attila Szucs <attila.szucs@gmail.com>
2016-05-15 08:42:38 +0200

    nobel app styling

--- nobel-app/routes/nobel.js
+++ nobel-app/routes/nobel.js
@@ -2,7 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 const cassandra = require('cassandra-driver')
-const client = new cassandra.Client({ contactPoints: ['seed1'], keyspace: 'ks1'});
+const client = new cassandra.Client({ contactPoints: ['node1'], keyspace: 'meetup'});
+
+/* GET by year. */
+router.get('/year/:year', function(req, res, next) {
+  var year = parseInt(req.params.year)
+  console.log("Year: " + year)
+  const cql = 'SELECT * FROM nobel_laureates WHERE year=?';
+  client.execute(cql, [year], {prepare: true}, function(err, result) {
+    renderResult(res, err, 'Laureates in ' + year, result)
+  });
+});
+
+/* GET by country. */
+router.get('/country/:code', function(req, res, next) {
+  var code = req.params.code.toUpperCase()
+  console.log("Country code: " + code)
+  const cql = 'SELECT * FROM nobel_laureates WHERE borncountrycode=?';
+  client.execute(cql, [code], {prepare: true}, function(err, result) {
+    renderResult(res, err, 'Laureates from ' + code, result)
+  });
+});
 
 function renderResult(res, err, title, result){
   if (err) {
@@ -13,23 +33,4 @@ function renderResult(res, err, title, result){
   }
 }
 
-/* GET by year. */
-router.get('/year/:year', function(req, res, next) {
-  var year = parseInt(req.params.year)
-  console.log("Year: " + year)
-  const query = 'SELECT * FROM nobel_laureates WHERE year=?';
-  client.execute(query, [year], {prepare: true}, function(err, result) {
-    renderResult(res, err, 'Laureates in ' + year, result)
-  });
-});
-
-/* GET by country. */
-router.get('/country/:code', function(req, res, next) {
-  var code = req.params.code.toUpperCase()
-  console.log("Country code: " + code)
-  const query = 'SELECT * FROM nobel_laureates WHERE borncountrycode=?';
-  client.execute(query, [code], {prepare: true}, function(err, result) {
-    renderResult(res, err, 'Laureates from ' + code, result)
-  });
-});
 module.exports = router;
