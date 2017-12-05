756427e725377538d85590eba51076f4808e426b
Attila Szucs <attila.szucs@flixbus.com>
2017-12-05 11:41:31 +0100

    Add nobelapp to docker-compose

--- nobel-app/routes/nobel.js
+++ nobel-app/routes/nobel.js
@@ -8,7 +8,7 @@ const client = new cassandra.Client({ contactPoints: ['node1'], keyspace: 'nobel
 router.get('/year/:year', function(req, res, next) {
   var year = parseInt(req.params.year)
   console.log("Year: " + year)
-  const cql = 'SELECT * FROM nobel_laureates WHERE year=?';
+  const cql = 'SELECT * FROM laureates WHERE year=?';
   client.execute(cql, [year], {prepare: true}, function(err, result) {
     renderResult(res, err, 'Laureates in ' + year, result, row => row.category)
   });
@@ -18,7 +18,7 @@ router.get('/year/:year', function(req, res, next) {
 router.get('/country/:code', function(req, res, next) {
   var code = req.params.code.toUpperCase()
   console.log("Country code: " + code)
-  const cql = 'SELECT * FROM nobel_laureates WHERE borncountrycode=?';
+  const cql = 'SELECT * FROM laureates WHERE borncountrycode=?';
   client.execute(cql, [code], {prepare: true}, function(err, result) {
     renderResult(res, err, 'Laureates from ' + code, result, row => row.year)
   });
